69df3b70b5ba0b75cfc1b9fa8a1406d45d10f46f
Luke A. Guest <laguest@archeia.com>
2018-04-05 18:13:58 +0100

    Keep consistent braces.

--- src/cpp/binding/b_base.cpp
+++ src/cpp/binding/b_base.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 // Thunk functions.
-extern "C" {
+extern "C"
+{
   extern const void Thunk_Base_Do_One(Ada_Base);  // Exported from Ada.
   extern const void Thunk_Base_Do_Two(Ada_Base);  // Exported from Ada.
 }
@@ -11,39 +12,47 @@ ThunkPtr_DoOne_Type B_Base::ThunkPtr_DoOne = Thunk_Base_Do_One;
 ThunkPtr_DoTwo_Type B_Base::ThunkPtr_DoTwo = Thunk_Base_Do_Two;
 
 // C++ Class methods.
-B_Base::B_Base(Ada_Base b) : Base(), Ada_Base_Self(b) {
+B_Base::B_Base(Ada_Base b) : Base(), Ada_Base_Self(b)
+{
   std::cout << "  (C++)\tB_Base::B_Base" << std::endl;
 }
 
-B_Base::B_Base(Ada_Base b, const int v) : Base(v), Ada_Base_Self(b) {
+B_Base::B_Base(Ada_Base b, const int v) : Base(v), Ada_Base_Self(b)
+{
   std::cout << "  (C++)\tB_Base::B_Base(" << v << ")" << std::endl;
 }
 
-B_Base::~B_Base() {
+B_Base::~B_Base()
+{
   std::cout << "  (C++)\tB_Base::~B_Base" << std::endl;
 }
 
-void B_Base::DoOne() {
+void B_Base::DoOne()
+{
   std::cout << "  (C++)\tB_Base::DoOne" << std::endl;
 
   (*ThunkPtr_DoOne)(Ada_Base_Self);
 }
 
-void B_Base::DoTwo() {
+void B_Base::DoTwo()
+{
   std::cout << "  (C++)\tB_Base::DoTwo" << std::endl;
 
   (*ThunkPtr_DoTwo)(Ada_Base_Self);
 }
 
 // C binding functions.
-extern "C" {
-  B_Base *Base_ctor(Ada_Base Ada_Self) {
+extern "C"
+{
+  B_Base *Base_ctor(Ada_Base Ada_Self)
+  {
     std::cout << "  (C)\tBase_ctor" << std::endl;
 
     return new B_Base(Ada_Self);
   }
 
-  void Base_dtor(B_Base *Self) {
+  void Base_dtor(B_Base *Self)
+  {
     std::cout << "  (C)\tBase_dtor" << std::endl;
 
     delete Self;
@@ -57,7 +66,8 @@ extern "C" {
 /*     Self->Base::DoOne(); */
 /*   } */
 
-  void Base_DoTwo(B_Base *Self) {
+  void Base_DoTwo(B_Base *Self)
+  {
     std::cout << "  (C)\tBase_DoTwo" << std::endl;
 
     // Call the base class' DoTwo(), not this class' version, as that would then dispatch to Ada causing an
@@ -65,13 +75,15 @@ extern "C" {
     Self->Base::DoTwo();
   }
 
-  int Base_Value(const B_Base *Self) {
+  int Base_Value(const B_Base *Self)
+  {
     std::cout << "  (C)\tBase_Value" << std::endl;
 
     return Self->Value();
   }
 
-  void Base_SetValue(B_Base *Self, const int v) {
+  void Base_SetValue(B_Base *Self, const int v)
+  {
     std::cout << "  (C)\tBase_SetValue" << std::endl;
 
     Self->SetValue(v);
--- src/cpp/binding/b_derived1.cpp
+++ src/cpp/binding/b_derived1.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 // Thunk functions.
-extern "C" {
+extern "C"
+{
   extern const void Thunk_Derived1_Do_One(Ada_Derived1);  // Exported from Ada.
   extern const void Thunk_Derived1_Do_Two(Ada_Derived1);  // Exported from Ada.
 }
@@ -11,45 +12,54 @@ ThunkPtr_DoOne_Type B_Derived1::ThunkPtr_DoOne = Thunk_Derived1_Do_One;
 ThunkPtr_DoTwo_Type B_Derived1::ThunkPtr_DoTwo = Thunk_Derived1_Do_Two;
 
 // C++ Class methods.
-B_Derived1::B_Derived1(Ada_Derived1 d) : Derived1(), Ada_Derived1_Self(d) {
+B_Derived1::B_Derived1(Ada_Derived1 d) : Derived1(), Ada_Derived1_Self(d)
+{
   std::cout << "  (C++)\tB_Derived1::B_Derived1" << std::endl;
 }
 
-B_Derived1::B_Derived1(Ada_Derived1 d, const int v) : Derived1(v), Ada_Derived1_Self(d) {
+B_Derived1::B_Derived1(Ada_Derived1 d, const int v) : Derived1(v), Ada_Derived1_Self(d)
+{
   std::cout << "  (C++)\tB_Derived1::B_Derived1(" << v << ")" << std::endl;
 }
 
-B_Derived1::~B_Derived1() {
+B_Derived1::~B_Derived1()
+{
   std::cout << "  (C++)\tB_Derived1::~B_Derived1" << std::endl;
 }
 
-void B_Derived1::DoOne() {
+void B_Derived1::DoOne()
+{
   std::cout << "  (C++)\tB_Derived1::DoOne" << std::endl;
 
   (*this->ThunkPtr_DoOne)(Ada_Derived1_Self);
 }
 
-void B_Derived1::DoTwo() {
+void B_Derived1::DoTwo()
+{
   std::cout << "  (C++)\tB_Derived1::DoTwo" << std::endl;
 
   (*this->ThunkPtr_DoTwo)(Ada_Derived1_Self);
 }
 
 // C binding functions.
-extern "C" {
-  B_Derived1 *Derived1_ctor(Ada_Derived1 Ada_Self) {
+extern "C"
+{
+  B_Derived1 *Derived1_ctor(Ada_Derived1 Ada_Self)
+  {
     std::cout << "  (C)\tDerived1_ctor" << std::endl;
 
     return new B_Derived1(Ada_Self);
   }
 
-  B_Derived1 *Derived1_ctor1(Ada_Derived1 Ada_Self, const int v) {
+  B_Derived1 *Derived1_ctor1(Ada_Derived1 Ada_Self, const int v)
+  {
     std::cout << "  (C)\tDerived1_ctor(" << v << ")" << std::endl;
 
     return new B_Derived1(Ada_Self, v);
   }
 
-  void Derived1_dtor(B_Derived1 *Self) {
+  void Derived1_dtor(B_Derived1 *Self)
+  {
     std::cout << "  (C)\tDerived1_dtor" << std::endl;
 
     delete Self;
@@ -57,7 +67,8 @@ extern "C" {
     Self = nullptr;
   }
 
-  void Derived1_DoOne(B_Derived1 *Self) {
+  void Derived1_DoOne(B_Derived1 *Self)
+  {
     std::cout << "  (C)\tDerived1_DoOne" << std::endl;
 
     // Call the base class' DoOne(), not this class' version, as that would then dispatch to Ada causing an
@@ -65,7 +76,8 @@ extern "C" {
     Self->Derived1::DoOne();
   }
 
-  void Derived1_DoTwo(B_Derived1 *Self) {
+  void Derived1_DoTwo(B_Derived1 *Self)
+  {
     std::cout << "  (C)\tDerived1_DoTwo" << std::endl;
 
     // Call the base class' DoTwo(), not this class' version, as that would then dispatch to Ada causing an
